a4b5dc9983453c6f34ddbd9faa22602432807286
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-01-26 10:58:37 +0500

    api

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def name():
     return 'Hello depoment'
+
+@app.route("/rout")
+def main():
+    return "Depolymint app"
+
 if __name__=="__main__":
     app.run(debug=True, port=4000)
