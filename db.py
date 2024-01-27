67fab822ce0c913d75bdbbd1ad49182af28e14ec
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-01-27 15:30:04 +0500

    db.py

--- db.py
+++ db.py
@@ -1,8 +1,17 @@
 from flask import Flask
-
+import telegram
 app = Flask(__name__)
+
+TOKEN = "6333225235:AAFbjxLb1QS7zZIW4maMZ4-CtRNybDdH6ys"
+
+bot = telegram.Bot(TOKEN)
+
+chat_id = "5466480235"
+
+
 @app.route("/")
 def rout():
+    bot.send_message(chat_id=chat_id,text="Har doimgdik 'Hello wordl'")
     return "Har doimgek 'Hello worlld!'"
 
 if __name__=="__main__":
