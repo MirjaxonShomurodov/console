a84555d1bd34817c1e36543620ece5d87cb0578a
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-01-28 18:53:03 +0500

    app

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask
 import telegram 
 
 app = Flask(__name__)
+
 TOKEN = "6333225235:AAFbjxLb1QS7zZIW4maMZ4-CtRNybDdH6ys"
+
 bot = telegram.Bot(TOKEN)
+
 chat_id = "5466480235"
+
 @app.route('/')
 def name():
+    
     bot.send_message(chat_id=chat_id,text="Har doimgdik 'Hello World'")
     return 'Hello deploymint'
 
