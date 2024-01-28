cd0a7df77b1692bd8beb20ca584e48c6705ccf66
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-01-28 16:59:32 +0500

    deploymint

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
+import telegram 
 
 app = Flask(__name__)
-
+TOKEN = "6333225235:AAFbjxLb1QS7zZIW4maMZ4-CtRNybDdH6ys"
+bot = telegram.Bot(TOKEN)
+chat_id = "5466480235"
 @app.route('/')
 def name():
-    return 'hi deploymint'
+    bot.send_message(chat_id=chat_id,text="Har doimgdik 'Hello World'")
+    return 'Hello deploymint'
 
-@app.route("/rout")
-def main():
-    return "Depolymint app"
+
 
 if __name__=="__main__":
     app.run(debug=True, port=4000)
