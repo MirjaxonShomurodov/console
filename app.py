f9efe0d40dbdaa5ccb59caa2af4f21d287242094
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-01-30 15:17:12 +0500

    text

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-from flask import Flask
+from flask import Flask,request
 import telegram 
 
 app = Flask(__name__)
 
 TOKEN = "6333225235:AAFbjxLb1QS7zZIW4maMZ4-CtRNybDdH6ys"
+URL = "http://mirjaxon.pythonanywhere.com/"
 
 bot = telegram.Bot(TOKEN)
 
 chat_id = "5466480235"
-@app.route('/')
+@app.route('/',methods = ['POST'])
 def main():
-  
-    bot.send_message(
-        chat_id=chat_id,
-        text=f"Hello, how are you, thank you for visiting our site "
-        )
+    data = request.get_json()
+    chat_id = data['message']['chat']['id']
+    bot.send_message(chat_id=chat_id,text=data['message']['text'])
+    
     return 'Hello deploymint!'
-
-
 
 if __name__=="__main__":
     app.run(debug=True, port=4000)
