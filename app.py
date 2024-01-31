49eb6c8dc004c14772e22c5fde1c15a4daae2572
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-02-01 00:28:59 +0500

    app.py

--- app.py
+++ app.py
@@ -10,31 +10,33 @@ bot = telegram.Bot(TOKEN)
 @app.route('/',methods = ['POST'])
 def main():
     data = request.get_json()
-    print(data)
     chat_id = data['message']['chat']['id']
-    if data['message'].get['message']!=None:   
+    print(data)
+    if data['message'].get('text')!=None:   
         bot.send_message(chat_id=chat_id,text=data['message']['text'])
-    elif data['message'].get['photo']!=None:
+    elif data['message'].get('photo')!=None:
         bot.send_photo(chat_id=chat_id,file_id=['message']['photo'][-1]['file_id'])
-    elif data['message'].get['audio']!=None:
-        bot.send_audio(chat_id=chat_id,audio = ['message']['audio']['file_id'])
-    elif ['message'].get['contact']!=None:
-        bot.send_contact(chat_id=chat_id,contact=['message']['contact'],full_name=['message']['from_user'])
-    elif data['message'].get['animation']!=None:
-        bot.send_animation(chat_id=chat_id,misc=['message']['animation'])
-    elif data['message'].get['dice']!=None:
-        bot.send_dice(chat_id=chat_id,text=['message']['dice'])
-    elif data['message'].get['location']!=None:
-        bot.send_location(chat_id=chat_id,text = ['message']['location'])
-    elif data['message'].get['sticker']!=None:
-        bot.send_sticker(chat_id=chat_id,text=['message']['sticker'])
-    elif data['message'].get['poll']!=None:
+    elif data['message'].get('audio')!=None:
+        bot.send_audio(chat_id,data['message']['audio']['file_id'],caption="Audio fayil.")
+    elif ['message'].get('contact')!=None:
+        bot.send_contact(chat_id,data['message']['contact']['phone_number'],data['message']['contact']['first_name'])
+    elif data['message'].get('animation')!=None:
+        bot.send_animation(chat_id=chat_id,animation=data['message']['animation'],duration=12)
+    elif data['message'].get('dice')!=None:
+        bot.send_dice(chat_id=chat_id,text=['message']['dice']['emoji'],disable_notification=True)
+    elif data['message'].get('location')!=None:
+        bot.send_location(chat_id=chat_id,latitude = ['message']['location']['latitude'],lonitude=data['message']['location']['lonitude'],heading=2,protect_content=True)
+    elif data['message'].get('sticker')!=None:
+        bot.send_sticker(chat_id=chat_id,text=['message']['sticker'],file_id = str(['file_id']),file_uniqua_id=['file_uniqua_id'],width=['width'],height=['height'])
+    elif data['message'].get('voice')!=None:
+        bot.send_voice(chat_id=chat_id, data = data['message']['voice']['file_id'])
+    elif data['message'].get('poll')!=None:
         bot.send_poll(chat_id=chat_id,is_anonymous=True)
-    elif data['message'].get['document']!=None:
-        bot.send_document(chat_id=chat_id,text = ['message']['document']['file_id'])
-    elif data['message'].get['video']!=None:
-        bot.send_video(chat_id=chat_id,video = ['message']['video']['file_id'])
+    elif data['message'].get('document')!=None:
+        bot.send_document(chat_id=chat_id,document =data['message']['document']['file_id'],capiton="Documitatsiya",protect_copntent=True)
+    elif data['message'].get('video')!=None:
+        bot.send_video(chat_id=chat_id,video = data['message']['video'][0]['file_id'])
     return 'Hello deploymint!'
 
 if __name__=="__main__":
-    app.run(debug=True, port=4000)
+    app.run(debug=True, port=8000)
