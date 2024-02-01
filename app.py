c24cd71d8aa08384a34f642d99129d539a5b7623
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-02-01 17:35:19 +0500

    poll

--- app.py
+++ app.py
@@ -13,26 +13,37 @@ def main():
     print(data)
     if data['message'].get('text')!=None:   
         bot.send_message(chat_id=chat_id,text=data['message']['text'])
+
     elif data['message'].get('photo')!=None:
-        bot.send_photo(chat_id=chat_id,file_id=['message']['photo'][-1]['file_id'])
+        bot.send_photo(chat_id=chat_id,photo=data['message']['photo'][0]['file_id'])
+
     elif data['message'].get('audio')!=None:
         bot.send_audio(chat_id,data['message']['audio']['file_id'],caption="Audio fayil.")
+
     elif ['message'].get('contact')!=None:
         bot.send_contact(chat_id,data['message']['contact']['phone_number'],data['message']['contact']['first_name'])
+
     elif data['message'].get('animation')!=None:
         bot.send_animation(chat_id=chat_id,animation=data['message']['animation'],duration=12)
+
     elif data['message'].get('dice')!=None:
-        bot.send_dice(chat_id=chat_id,text=['message']['dice']['emoji'],disable_notification=True)
+        bot.send_dice(chat_id=chat_id,emoji=data['message']['dice']['emoji'],disable_notification=True)
+
     elif data['message'].get('location')!=None:
-        bot.send_location(chat_id=chat_id,latitude = ['message']['location']['latitude'],lonitude=data['message']['location']['lonitude'],heading=2,protect_content=True)
+        bot.send_location(chat_id=chat_id,latitude = data['message']['location']['latitude'],longitude=data['message']['location']['longitude'],heading=2,protect_content=True)
+
     elif data['message'].get('sticker')!=None:
-        bot.send_sticker(chat_id=chat_id,text=['message']['sticker'],file_id = str(['file_id']),file_uniqua_id=['file_uniqua_id'],width=['width'],height=['height'])
+        bot.send_sticker(chat_id=chat_id,text=data['message']['sticker'],file_id = str(['file_id']),file_uniqua_id=data['file_uniqua_id'],width=['width'],height=['height'])
+
     elif data['message'].get('voice')!=None:
         bot.send_voice(chat_id=chat_id, data = data['message']['voice']['file_id'])
+
     elif data['message'].get('poll')!=None:
-        bot.send_poll(chat_id=chat_id,is_anonymous=True)
+        bot.send_poll(chat_id=chat_id,question = data['message']['poll']['question'],options = data['message']['poll']['options'])
+
     elif data['message'].get('document')!=None:
-        bot.send_document(chat_id=chat_id,document =data['message']['document']['file_id'],capiton="Documitatsiya",protect_copntent=True)
+        bot.send_document(chat_id=chat_id,document =data['message']['document']['file_id'],caption="Documitatsiya",protect_copntent=True)
+
     elif data['message'].get('video')!=None:
         bot.send_video(chat_id=chat_id,video = data['message']['video'][0]['file_id'])
     return 'Hello deploymint!'
