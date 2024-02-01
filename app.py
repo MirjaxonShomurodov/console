c03387b014e9f19f3ec916da7f3e1a5c0d29e65d
MirjaxonShomurodov <mirjaxon457@gmail.com>
2024-02-01 16:41:39 +0500

    txt api

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask,request
-import telegram 
+import telegram
 app = Flask(__name__)
 
 TOKEN = "6333225235:AAFbjxLb1QS7zZIW4maMZ4-CtRNybDdH6ys"
 
-URL = "https://mirjaxon.pythonanywhere.com/"
 bot = telegram.Bot(TOKEN)
 
 @app.route('/',methods = ['POST'])
